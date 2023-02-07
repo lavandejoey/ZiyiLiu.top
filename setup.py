7247448ce41b86848a5e90f70ac646cbb7723bc6
LAVANDEJOEY <48612765+lavandejoey@users.noreply.github.com>
2023-02-07 17:34:15 +0800

    20230207-setup code

--- setup.py
+++ setup.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python
-__author__ = "Rob Knight, Gavin Huttley, and Peter Maxwell"
-__copyright__ = "Copyright 2007, The Cogent Project"
-__credits__ = ["Rob Knight", "Peter Maxwell", "Gavin Huttley",
-               "Matthew Wakefield"]
-__license__ = "GPL"
-__version__ = "1.0.1"
-__maintainer__ = "Rob Knight"
-__email__ = "rob@spot.colorado.edu"
-__status__ = "Production"
+__author__ = "lavandejoey, Ziyi LIU"
+__copyright__ = "Copyright 2021-2023"
+__license__ = "Apache 2.0"
+__version__ = "0.0.1"
+__maintainer__ = "lavandejoey"
+__email__ = "lavandejoey@outlook.com"
 # standard library
+import os
 # 3rd party packages
+from flask import Flask
+import flask_sqlalchemy
+from wekzeug.contrib.fixers import ProxyFix
 # local source
+import mainApp
+
+app=create_app()
+
+def run():
+    debug=os.environ.get("APP_DEBUG",True)
+    host=os.environ.get("APP_HOST","0.0.0.0")
+    port=os.environ.get("APP_PORT",8080)
+
+    app.run(debug=debug,host=host,port=port)
+
+wsgi=ProxyFix(app.wsgi_app)
+
+if __name__=='__main__':
+    print(app)#run()
