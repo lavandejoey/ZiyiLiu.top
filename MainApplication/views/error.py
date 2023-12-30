f50bcb28b1a2053c26575364f12cbc7825bed2b4
LAVANDEJOEY <48612765+lavandejoey@users.noreply.github.com>
2023-12-30 19:34:15 +0800

    Deving -> main (#22)
    * 20231015 sitemap repairing
    
    * 20231019 Update scss styles and animation; connecting to nas (as subdomain).
    
    * 20231102 Initialize the token authed OpenAPI
    
    * 20231102 Update API sec
    
    * 20231102 Update for sec
    
    * 20231130 Update db api
    
    * 20231229 Fix the sign up bugs.
    
    * 20231229 Fix the sign up bugs.
    
    * 20231230 Fix pages' title for multi-lang. HMI coursework submission version.
    
    ---------
    
    Co-authored-by: Joshua Ziyi LIU <joshuazyl@localhost.localdomain>

--- MainApplication/views/error.py
+++ MainApplication/views/error.py
@@ -10,6 +10,7 @@ __email__ = "lavandejoey@outlook.com"
 # standard library
 # 3rd party packages
 from flask import Blueprint, render_template
+from flask_babel import gettext
 
 # local source
 
@@ -23,19 +24,31 @@ error_blueprint = Blueprint(name="error", import_name=__name__, static_folder="s
 @error_blueprint.app_errorhandler(404)  # 404 Not Found
 @error_blueprint.app_errorhandler(405)  # 405 Method Not Allowed
 def handle_error(error):
-    return render_template('error/error.html'), error.code
+    return render_template('error/error.html',
+                           title=gettext("Error"),
+                           error_code=error.code,
+                           error_name=error.name), error.code
 
 
 @error_blueprint.app_errorhandler(500)
 def internal_error(error):
-    return "500"
+    return render_template('error/error.html',
+                           title=gettext("Internal Server Error"),
+                           error_code=error.code,
+                           error_name=error.name), error.code
 
 
 @error_blueprint.app_errorhandler(501)
 def not_implemented(error):
-    return "501"
+    return render_template('error/error.html',
+                           title=gettext("Not Implemented"),
+                           error_code=error.code,
+                           error_name=error.name), error.code
 
 
 @error_blueprint.app_errorhandler(502)
 def bad_gateway(error):
-    return "502"
+    return render_template('error/error.html',
+                           title=gettext("Bad Gateway"),
+                           error_code=error.code,
+                           error_name=error.name), error.code
